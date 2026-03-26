fb6a575365b38a94264e0e93860302e395110412
biruk2116 <brukbela61@gmail.com>
2026-03-26 22:11:02 +0300

    tailwind css setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-
-
-
 function App() {
-  
-
   return (
-    <>
-      
-    </>
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-white text-4xl font-bold">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
   )
 }
 
